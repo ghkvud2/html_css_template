e19f992b3222907af8d04cf06c23a6f8f71ee216
ghkvud2 <ghkvud2@naver.com>
2020-12-01 23:49:14 +0900

    셀렉트박스 검색기능 추가

--- select-box/select-box.js
+++ select-box/select-box.js
@@ -1,9 +1,17 @@
 const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 const optionsList = document.querySelectorAll(".option");
+const searchBox = document.querySelector(".search-box input");
 
 selected.addEventListener("click", () => {
     optionsContainer.classList.toggle("active");
+
+    searchBox.value = "";
+    filterList("");
+    
+    if(optionsContainer.classList.contains("active")){
+        searchBox.focus();
+    }
 });
 
 optionsList.forEach(option => {
@@ -12,3 +20,19 @@ optionsList.forEach(option => {
         optionsContainer.classList.remove("active");
     });
 });
+
+searchBox.addEventListener("keyup", (event) => {
+    filterList(event.target.value);
+});
+
+const filterList = (inputValue) => {
+    inputValue = inputValue.toUpperCase();
+    optionsList.forEach(option => {
+        let label = option.firstElementChild.nextElementSibling.textContent.toUpperCase();
+        if(label.indexOf(inputValue) != -1){
+            option.style.display = "block";
+        }else{
+            option.style.display = "none";
+        }
+    });
+};
